6927d4cd49e903f38e159a57c9a7c94a28d9d36c
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-18 00:19:45 +0200

    Update ChoixActionsAjouterSpecialite.java

--- ChoixActionsAjouterSpecialite.java
+++ ChoixActionsAjouterSpecialite.java
@@ -1,39 +1,32 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.SpringLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import controleur.Ctrl;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
-public class ChoixActionsAjouterSpecialite extends JFrame {
-
-	private JPanel contentPane;
-
+public class ChoixActionsAjouterSpecialite extends View {
 	/**
-	 * Launch the application.
+	 * 
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ChoixActionsAjouterSpecialite frame = new ChoixActionsAjouterSpecialite();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPanel panel;
+	private JLabel lblLaSpcialitDu;
+	private JButton btnOui;
+	private JButton btnNon;
+	public JList listSpecialite;
+	private JButton btnRetour2;
 	/**
 	 * Create the frame.
 	 */
-	public ChoixActionsAjouterSpecialite() {
+	public ChoixActionsAjouterSpecialite(Ctrl ctrl, String[] tableauSpecialite) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -41,27 +34,51 @@ public class ChoixActionsAjouterSpecialite extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		SpringLayout sl_panel = new SpringLayout();
 		panel.setLayout(sl_panel);
 		
-		JLabel lblLaSpcialitDu = new JLabel("La sp\u00E9cialit\u00E9 du m\u00E9decin \u00E0 ajouter est-elle dans la liste suivante?");
+		lblLaSpcialitDu = new JLabel("La sp\u00E9cialit\u00E9 du m\u00E9decin \u00E0 ajouter est-elle dans la liste suivante?");
 		sl_panel.putConstraint(SpringLayout.NORTH, lblLaSpcialitDu, 33, SpringLayout.NORTH, panel);
 		sl_panel.putConstraint(SpringLayout.WEST, lblLaSpcialitDu, 10, SpringLayout.WEST, panel);
 		panel.add(lblLaSpcialitDu);
 		
-		JButton btnOui = new JButton("OUI");
-		sl_panel.putConstraint(SpringLayout.WEST, btnOui, -166, SpringLayout.EAST, panel);
-		sl_panel.putConstraint(SpringLayout.SOUTH, btnOui, -137, SpringLayout.SOUTH, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, btnOui, -99, SpringLayout.EAST, panel);
+		btnOui = new JButton("OUI");
 		panel.add(btnOui);
 		
-		JButton btnNon = new JButton("NON");
-		sl_panel.putConstraint(SpringLayout.NORTH, btnNon, 25, SpringLayout.SOUTH, btnOui);
+		btnNon = new JButton("NON");
+		sl_panel.putConstraint(SpringLayout.EAST, btnOui, 0, SpringLayout.EAST, btnNon);
+		sl_panel.putConstraint(SpringLayout.NORTH, btnNon, 140, SpringLayout.NORTH, panel);
 		sl_panel.putConstraint(SpringLayout.WEST, btnNon, 254, SpringLayout.WEST, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, btnNon, 0, SpringLayout.EAST, btnOui);
+		sl_panel.putConstraint(SpringLayout.EAST, btnNon, -99, SpringLayout.EAST, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, btnOui, 0, SpringLayout.WEST, btnNon);
+		sl_panel.putConstraint(SpringLayout.SOUTH, btnOui, -24, SpringLayout.NORTH, btnNon);
 		panel.add(btnNon);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		sl_panel.putConstraint(SpringLayout.NORTH, scrollPane, 6, SpringLayout.SOUTH, lblLaSpcialitDu);
+		sl_panel.putConstraint(SpringLayout.WEST, scrollPane, 59, SpringLayout.WEST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, scrollPane, 79, SpringLayout.SOUTH, btnNon);
+		sl_panel.putConstraint(SpringLayout.EAST, scrollPane, 222, SpringLayout.WEST, panel);
+		panel.add(scrollPane);
+		
+		listSpecialite = new JList(tableauSpecialite);
+		
+		scrollPane.setViewportView(listSpecialite);
+		
+		btnRetour2 = new JButton("Retour");
+		sl_panel.putConstraint(SpringLayout.SOUTH, btnRetour2, -10, SpringLayout.SOUTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, btnRetour2, -10, SpringLayout.EAST, panel);
+		panel.add(btnRetour2);
 	}
-
+	
+	public void assignListener(Ctrl ctrl){
+		this.btnOui.setActionCommand("choix_actions_ajouter_oui");
+		this.btnOui.addActionListener(ctrl);
+		this.btnNon.setActionCommand("choix_actions_ajouter_non");
+		this.btnNon.addActionListener(ctrl);
+		this.btnRetour2.setActionCommand("choix_actions_ajouter_retour2");
+		this.btnRetour2.addActionListener(ctrl);
+	}
 }
