62f63b2387dec6a9553a3ec7e9ce9f4022030659
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-18 00:22:15 +0200

    Update specialite.java

--- specialite.java
+++ specialite.java
@@ -1,24 +1,36 @@
-package classe;
+package model;
 
 import java.util.ArrayList;
+import controleur.AccesBD;
 
 public class specialite {
 	private int identifiant;
 	private String libelle;
 	public static ArrayList<specialite> toutesLesSpecialites=new ArrayList<specialite>();
 	
-	public specialite(int identifiant,String libelle){
-		this.identifiant=identifiant;
+	public specialite(String libelle){
 		this.libelle=libelle;
+		
+		try {
+			String [][] tableau = AccesBD.ajoutSpecialite(libelle);
+			for (int i = 0; i < tableau.length; i++) {
+				String identifiant = tableau[0][0];
+				this.identifiant = Integer.parseInt(identifiant);
+				}
+			}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+		
 		toutesLesSpecialites.add(this);
-	}
+		}
 	
 	public int getIdentifiant(){
 		return identifiant;
-	}
+		}
 	
 	public String getLibelle(){
 		return libelle;
-	}
-	
+		}
 }
