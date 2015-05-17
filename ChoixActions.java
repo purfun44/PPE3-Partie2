242c4042c9dd101ef4d290160f9b01a724f9b0bd
purfun44 <sio.pasquier.timothee@gmail.com>
2015-05-18 00:17:25 +0200

    Update ChoixActions.java

--- ChoixActions.java
+++ ChoixActions.java
@@ -1,40 +1,31 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.SpringLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import controleur.Ctrl;
 
-public class ChoixActions extends JFrame {
-
-	private JPanel contentPane;
+public class ChoixActions extends View {
 
 	/**
-	 * Launch the application.
+	 * 
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ChoixActions frame = new ChoixActions();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPanel panel;
+	private JLabel lblQuelleActionChoisir; 
+	private JButton btnAjouter;
+	private JButton btnModifier;
+	private JButton btnSupprimer;
+	private JButton btnRetour;
 	/**
 	 * Create the frame.
 	 */
-	public ChoixActions() {
+	public ChoixActions(Ctrl ctrl) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -42,31 +33,46 @@ public class ChoixActions extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		SpringLayout sl_panel = new SpringLayout();
 		panel.setLayout(sl_panel);
 		
-		JLabel lblQuelleActionChoisir = new JLabel("Quelle action choisir?");
+		lblQuelleActionChoisir = new JLabel("Quelle action choisir?");
 		sl_panel.putConstraint(SpringLayout.NORTH, lblQuelleActionChoisir, 40, SpringLayout.NORTH, panel);
 		sl_panel.putConstraint(SpringLayout.WEST, lblQuelleActionChoisir, 65, SpringLayout.WEST, panel);
 		panel.add(lblQuelleActionChoisir);
 		
-		JButton btnAjouter = new JButton("Ajouter");
+		btnAjouter = new JButton("Ajouter");
 		sl_panel.putConstraint(SpringLayout.NORTH, btnAjouter, 24, SpringLayout.SOUTH, lblQuelleActionChoisir);
 		sl_panel.putConstraint(SpringLayout.WEST, btnAjouter, 0, SpringLayout.WEST, lblQuelleActionChoisir);
 		panel.add(btnAjouter);
 		
-		JButton btnModifier = new JButton("Modifier");
+		btnModifier = new JButton("Modifier");
 		sl_panel.putConstraint(SpringLayout.NORTH, btnModifier, 6, SpringLayout.SOUTH, btnAjouter);
 		sl_panel.putConstraint(SpringLayout.WEST, btnModifier, 0, SpringLayout.WEST, lblQuelleActionChoisir);
 		sl_panel.putConstraint(SpringLayout.EAST, btnModifier, 0, SpringLayout.EAST, btnAjouter);
 		panel.add(btnModifier);
 		
-		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer = new JButton("Supprimer");
 		sl_panel.putConstraint(SpringLayout.NORTH, btnSupprimer, 6, SpringLayout.SOUTH, btnModifier);
 		sl_panel.putConstraint(SpringLayout.EAST, btnAjouter, 0, SpringLayout.EAST, btnSupprimer);
 		sl_panel.putConstraint(SpringLayout.WEST, btnSupprimer, 0, SpringLayout.WEST, lblQuelleActionChoisir);
 		panel.add(btnSupprimer);
+		
+		btnRetour = new JButton("Retour");
+		sl_panel.putConstraint(SpringLayout.SOUTH, btnRetour, -10, SpringLayout.SOUTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, btnRetour, -10, SpringLayout.EAST, panel);
+		panel.add(btnRetour);
+	}
+	public void assignListener(Ctrl ctrl){
+		this.btnAjouter.setActionCommand("choix_actions_ajouter");
+		this.btnAjouter.addActionListener(ctrl);
+		this.btnModifier.setActionCommand("choix_actions_modifier");
+		this.btnModifier.addActionListener(ctrl);
+		this.btnSupprimer.setActionCommand("choix_actions_supprimer");
+		this.btnSupprimer.addActionListener(ctrl);
+		this.btnRetour.setActionCommand("choix_actions_retour");
+		this.btnRetour.addActionListener(ctrl);
 	}
 }
